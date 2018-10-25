03fa9a9ddd10ee5207d03e08eb18cd4c45e62653
JeeHwan Kim <JeeHwanKim@JeeHwans-MacBook-Air.local>
2018-10-24 23:02:59 -0400

    Rainbow colors / gradient colors worked!
    Did a lot of trials to figure out how rainbow colors worked - also for the second mandelbrot image, I tried to produce a gradient color between pink and red. It worked nicely!

--- MANDELBROT/m2.py
+++ MANDELBROT/m2.py
@@ -1,16 +1,15 @@
 from PIL import Image
 import math as m, colorsys as c
-global color, imgx, imgy, xmin, xmax, ymin, ymax
+from decimal import Decimal
 
 imgx, imgy = 512, 512
 
-# xmin, xmax = -0.5438500237651169, -0.5431959995767102
-# ymin, ymax = 0.6137094649166102, 0.614363489105169
-
-xmin, xmax = 0.13501, 0.13506
-ymin, ymax = 0.63751, 0.63756
+# MANDELBROT 1
 
 image = Image.new("RGB",(imgx, imgy))
+
+xmin, xmax = -0.8638, -0.8614
+ymin, ymax = 0.2637, 0.2659
 
 for i in range(imgx):
 	for j in range(imgy):
@@ -24,25 +23,69 @@ for i in range(imgx):
 
 		while True:
 			if sum == 256:
-				# maximum iteration is 100
-				a = c.hsv_to_rgb(1, 0.1, 1.0)
+				a = c.hsv_to_rgb(0.5, 0.2, 200)
 				image.putpixel((i, j), (int(a[0]), int(a[1]), int(a[2])))
 				break
+				# max iteration: 256
 			elif m.pow(z[0], 2) + m.pow(z[1], 2) >= 4:
-				b = c.hsv_to_rgb((1 / 64 * sum)/255, (sum / 10)/255, 1.0)
-				image.putpixel((i, j), (int(b[0]*255), int(b[1]*255), int(b[2]*255)))
-				# checking if z escaped
+				b = c.hsv_to_rgb(sum / 8 - int(sum / 8), sum / 8 - int(sum / 8), 200)
+				# repeating rainbow colors with varying saturation
+				image.putpixel((i, j), (int(b[0]), int(b[1]), int(b[2])))
+				# check if z escaped
 				break
 			else:
 				temp = z[0]
 				z[0] = m.pow(z[0], 2) - m.pow(z[1], 2)
 				z[1] = 2 * temp * z[1]
-				# calculated z squared
+				# calculate z * z
 
 				z[0] = z[0] + x
 				z[1] = z[1] + y
-				# calculated z + c
+				# calculate z + c
 
 				sum = sum + 1
 
-image.save("MANDELBROT4.png", "PNG")
+image.save("JeeHwan_M1.png", "PNG")
+
+# MANDELBROT 2
+
+# image = Image.new("RGB",(imgx, imgy))
+
+xmin, xmax = -0.45676176, -0.45669549
+ymin, ymax = 0.58401132, 0.58407759
+
+for i in range(imgx):
+	for j in range(imgy):
+
+		x = i * (xmax - xmin) / (imgx - 1) + xmin
+		y = j * (ymax - ymin) / (imgy - 1) + ymin
+
+		sum = 1
+
+		z = [x, y]
+
+		while True:
+			if sum == 256:
+				a = c.hsv_to_rgb(0.5, 0.2, 200)
+				image.putpixel((i, j), (int(a[0]), int(a[1]), int(a[2])))
+				break
+				# max iteration: 256
+			elif m.pow(z[0], 2) + m.pow(z[1], 2) >= 4:
+				b = c.hsv_to_rgb(0.925 + sum / 64 - int(sum / 64 / 0.047) * 0.047 , sum / 64 - int(sum / 64), 210 - sum * 100 / 255)
+				# gradient color from pink to red with varying value
+				image.putpixel((i, j), (int(b[0]), int(b[1]), int(b[2])))
+				# check if z escaped
+				break
+			else:
+				temp = z[0]
+				z[0] = m.pow(z[0], 2) - m.pow(z[1], 2)
+				z[1] = 2 * temp * z[1]
+				# calculate z * z
+
+				z[0] = z[0] + x
+				z[1] = z[1] + y
+				# calculate z + c
+
+				sum = sum + 1
+
+image.save("JeeHwan_M2.png", "PNG")
