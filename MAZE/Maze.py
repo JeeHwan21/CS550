92263b311a1d7cf5d5340620a7d015195efa92be
JeeHwan Kim <JeeHwanKim@JeeHwans-MacBook-Air.local>
2018-11-08 11:07:14 -0500

    Tried to figure out how to make use of frontiers

--- MAZE/Maze.py
+++ MAZE/Maze.py
@@ -1,9 +1,34 @@
-# ├, ┼, ┤, ┌, ┬, ┐, └, ┴, ┘
+import random as r
 
-grid = [["┌", "─", "┬", "─", "┬", "─", "┐"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["└", "─", "┴", "─", "┴", "─", "┘"]]
+# ├, ┼, ┤, ┌, ┬, ┐, └, ┴, ┘, ─
 
-for i in grid:
-	for j in i:
-		print(j, end = "")
-	print()
+agrid = [["┌", "─", "┬", "─", "┬", "─", "┐"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["└", "─", "┴", "─", "┴", "─", "┘"]]
 
+# for i in agrid:
+# 	for j in i:
+# 		print(j, end = "")
+# 	print()
+
+cgrid = [["D"] * (3 + 2) for x in range(3 + 2)]
+
+igrid = [["D"] * (3 + 2) for x in range(3 + 2)]
+
+for i in range(1, 3 + 1):
+	for j in range(1, 3 + 1):
+		igrid[i][j] = 0
+
+for x in igrid:
+	print(*x)
+
+
+
+def frontier(i, j):
+	if igrid[i][j] == "D" and igrid[i - 1][j] != "D" and igrid[i + 1][j] != "D" and igrid[i][j + 1] != "D" and igrid[i][j - 1] == "D":
+
+	if
+
+x = r.randint(1, 3)
+y = r.randint(1, 3)
+
+while True:
+	igrid[x][y] = D
