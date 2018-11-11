3598a15f3070c60adc0d030fcc74166fba68285d
JeeHwan Kim <JeeHwanKim@JeeHwans-MacBook-Air.local>
2018-11-11 17:39:16 -0500

    Finished my demo maze!!!
    So much errorchecking, but I finally finished a 3 x 3 grid maze! Just need to allow user input for dimensions...

--- MAZE/Maze.py
+++ MAZE/Maze.py
@@ -1,6 +1,6 @@
 import random as r
 
-# ├, ┼, ┤, ┌, ┬, ┐, └, ┴, ┘, ─
+# ├, ┼, ┤, ┌, ┬, ┐, └, ┴, ┘, ─, ╷, ╵, │, ╴, ╶
 
 agrid = [["┌", "─", "┬", "─", "┬", "─", "┐"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["├", "─", "┼", "─", "┼", "─", "┤"], ["└", "─", "┴", "─", "┴", "─", "┘"]]
 
@@ -9,15 +9,149 @@ for i in agrid:
 		print(j, end = "")
 	print()
 
-left = []
+for i in agrid:
+	print(*i)
 
-cgrid = [["B"] * (3 + 2) for x in range(3 + 2)]
+left = []
 
 igrid = [["B"] * (3 + 2) for x in range(3 + 2)]
 
 for i in range(1, 3 + 1):
 	for j in range(1, 3 + 1):
 		igrid[i][j] = 0
+
+def parentontop(i, j):
+	agrid[i - 1][j * 2 - 1] = " "
+
+	if agrid[i - 1][j * 2 - 2] == "├":
+		agrid[i - 1][j * 2 - 2] = "│"
+	elif agrid[i - 1][j * 2 - 2] == "┬":
+		agrid[i - 1][j * 2 - 2] = "┐"
+	elif agrid[i - 1][j * 2 - 2] == "┼":
+		agrid[i - 1][j * 2 - 2] = "┤"	
+	elif agrid[i - 1][j * 2 - 2] == "└":
+		agrid[i - 1][j * 2 - 2] = "╵"
+	elif agrid[i - 1][j * 2 - 2] == "┴":
+		agrid[i - 1][j * 2 - 2] = "┘"
+	elif agrid[i - 1][j * 2 - 2] == "┌":
+		agrid[i - 1][j * 2 - 2] = "╷"
+	elif agrid[i - 1][j * 2 - 2] == "╶":
+		agrid[i - 1][j * 2 - 2] = " "
+
+	if agrid[i - 1][j * 2] == "┤":
+		agrid[i - 1][j * 2] = "│"
+	elif agrid[i - 1][j * 2] == "┬":
+		agrid[i - 1][j * 2] = "┌"
+	elif agrid[i - 1][j * 2] == "┴":
+		agrid[i - 1][j * 2] = "└"
+	elif agrid[i - 1][j * 2] == "┼":
+		agrid[i - 1][j * 2] = "├"	
+	elif agrid[i - 1][j * 2] == "┘":
+		agrid[i - 1][j * 2] = "╵"
+	elif agrid[i - 1][j * 2] == "┐":
+		agrid[i - 1][j * 2] = "╷"
+	elif agrid[i - 1][j * 2] == "╴":
+		agrid[i - 1][j * 2] = " "
+
+def parentonbottom(i, j):
+	agrid[i][j * 2 - 1] = " "
+
+	if agrid[i][j * 2 - 2] == "├":
+		agrid[i][j * 2 - 2] = "│"
+	elif agrid[i][j * 2 - 2] == "┬":
+		agrid[i][j * 2 - 2] = "┐"
+	elif agrid[i][j * 2 - 2] == "┼":
+		agrid[i][j * 2 - 2] = "┤"	
+	elif agrid[i][j * 2 - 2] == "└":
+		agrid[i][j * 2 - 2] = "╵"
+	elif agrid[i][j * 2 - 2] == "┴":
+		agrid[i][j * 2 - 2] = "┘"
+	elif agrid[i][j * 2 - 2] == "┌":
+		agrid[i][j * 2 - 2] = "╷"
+	elif agrid[i][j * 2 - 2] == "╶":
+		agrid[i][j * 2 - 2] = " "
+
+	if agrid[i][j * 2] == "┤":
+		agrid[i][j * 2] = "│"
+	elif agrid[i][j * 2] == "┬":
+		agrid[i][j * 2] = "┌"
+	elif agrid[i][j * 2] == "┴":
+		agrid[i][j * 2] = "└"
+	elif agrid[i][j * 2] == "┼":
+		agrid[i][j * 2] = "├"		
+	elif agrid[i][j * 2] == "┘":
+		agrid[i][j * 2] = "╵"
+	elif agrid[i][j * 2] == "┐":
+		agrid[i][j * 2] = "╷"
+	elif agrid[i][j * 2] == "╴":
+		agrid[i][j * 2] = " "
+
+# ├, ┼, ┤, ┌, ┬, ┐, └, ┴, ┘, ─, ╷, ╵, │, ╴, ╶
+
+def parentonleft(i, j):
+
+	if agrid[i - 1][j * 2 - 2] == "├":
+		agrid[i - 1][j * 2 - 2] = "└"
+	elif agrid[i - 1][j * 2 - 2] == "┼":
+		agrid[i - 1][j * 2 - 2] = "┴"
+	elif agrid[i - 1][j * 2 - 2] == "┤":
+		agrid[i - 1][j * 2 - 2] = "┘"
+	elif agrid[i - 1][j * 2 - 2] == "┌":
+		agrid[i - 1][j * 2 - 2] = "╶"
+	elif agrid[i - 1][j * 2 - 2] == "┬":
+		agrid[i - 1][j * 2 - 2] = "─"
+	elif agrid[i - 1][j * 2 - 2] == "┐":
+		agrid[i - 1][j * 2 - 2] = "╴"
+	elif agrid[i - 1][j * 2 - 2] == "╷":
+		agrid[i - 1][j * 2 - 2] = " "
+
+	if agrid[i][j * 2 - 2] == "├":
+		agrid[i][j * 2 - 2] = "┌"
+	elif agrid[i][j * 2 - 2] == "┼":
+		agrid[i][j * 2 - 2] = "┬"
+	elif agrid[i][j * 2 - 2] == "┤":
+		agrid[i][j * 2 - 2] = "┐"
+	elif agrid[i][j * 2 - 2] == "└":
+		agrid[i][j * 2 - 2] = "╶"
+	elif agrid[i][j * 2 - 2] == "┴":
+		agrid[i][j * 2 - 2] = "─"
+	elif agrid[i][j * 2 - 2] == "┘":
+		agrid[i][j * 2 - 2] = "╴"
+	elif agrid[i][j * 2 - 2] == "╵":
+		agrid[i][j * 2 - 2] = " "
+
+def parentonright(i, j):
+
+	if agrid[i - 1][j * 2] == "├":
+		agrid[i - 1][j * 2] = "└"
+	elif agrid[i - 1][j * 2] == "┼":
+		agrid[i - 1][j * 2] = "┴"
+	elif agrid[i - 1][j * 2] == "┤":
+		agrid[i - 1][j * 2] = "┘"
+	elif agrid[i - 1][j * 2] == "┌":
+		agrid[i - 1][j * 2] = "╶"
+	elif agrid[i - 1][j * 2] == "┬":
+		agrid[i - 1][j * 2] = "─"
+	elif agrid[i - 1][j * 2] == "┐":
+		agrid[i - 1][j * 2] = "╴"
+	elif agrid[i - 1][j * 2] == "╷":
+		agrid[i - 1][j * 2] = " "
+
+	if agrid[i][j * 2] == "├":
+		agrid[i][j * 2] = "┌"
+	elif agrid[i][j * 2] == "┼":
+		agrid[i][j * 2] = "┬"
+	elif agrid[i][j * 2] == "┤":
+		agrid[i][j * 2] = "┐"
+	elif agrid[i][j * 2] == "└":
+		agrid[i][j * 2] = "╶"
+	elif agrid[i][j * 2] == "┴":
+		agrid[i][j * 2] = "─"
+	elif agrid[i][j * 2] == "┘":
+		agrid[i][j * 2] = "╴"
+	elif agrid[i][j * 2] == "╵":
+		agrid[i][j * 2] = " "
+
 
 def frontier(i, j):
 
@@ -45,27 +179,55 @@ def frontier(i, j):
 		if [i, j - 1] not in left:
 			left.append([i, j - 1])
 
-	for x in igrid:
-		print(*x)
+	print(left)
+
+	randleftover = r.randint(0, len(left) - 1)
+
+	temp = left[randleftover]
+
+	left.remove(left[randleftover])  # https://stackoverflow.com/questions/2793324/is-there-a-simple-way-to-delete-a-list-element-by-value
 
 	print(left)
 
-	random = r.randint(0, len(left) - 1)
-
-	temp = left[random]
-
-	if len(left) == 1:
-		for m in range(3 + 2):
-			for n in range(3 + 2):
-				if igrid[m][n] == "F":
-					igrid[m][n] = "D"
-
 	for x in igrid:
 		print(*x)
 
-	left.remove(left[random])
-
 	if len(left) > 0:
+
+		parentoptions = []
+
+		if igrid[temp[0] - 1][temp[1]] == "D":
+			parentoptions.append(1)
+
+		if igrid[temp[0] + 1][temp[1]] == "D":
+			parentoptions.append(2)
+
+		if igrid[temp[0]][temp[1] - 1] == "D":
+			parentoptions.append(3)
+
+		if igrid[temp[0]][temp[1] + 1] == "D":
+			parentoptions.append(4)
+
+		print(parentoptions)
+
+		ran = r.randint(0, len(parentoptions) - 1)
+
+		if parentoptions[ran] == 1:
+			parentontop(temp[0], temp[1])
+
+		elif parentoptions[ran] == 2:
+			parentonbottom(temp[0], temp[1])
+
+		elif parentoptions[ran] == 3:
+			parentonleft(temp[0], temp[1])
+
+		elif parentoptions[ran] == 4:
+			parentonright(temp[0], temp[1])
+
+		for i in agrid:
+			for j in i:
+				print(j, end = "")
+			print()
 
 		igrid[temp[0]][temp[1]] = "D"
 
@@ -83,7 +245,41 @@ igrid[x][y] = "D"
 
 frontier(x, y)
 
-# print(left)
+for i in range(1, 3 + 1):
+	for j in range(1, 3 + 1):
+		if igrid[i][j] == "F":
+			
+			parentoptions = []
 
-# for x in igrid:
-# 	print(*x)
+			if igrid[i - 1][j] == "D":
+				parentoptions.append(1)
+
+			if igrid[i + 1][j] == "D":
+				parentoptions.append(2)
+
+			if igrid[i][j - 1] == "D":
+				parentoptions.append(3)
+
+			if igrid[i][j + 1] == "D":
+				parentoptions.append(4)
+
+			print(parentoptions)
+
+			ran = r.randint(0, len(parentoptions) - 1)
+
+			if parentoptions[ran] == 1:
+				parentontop(i, j)
+
+			elif parentoptions[ran] == 2:
+				parentonbottom(i, j)
+
+			elif parentoptions[ran] == 3:
+				parentonleft(i, j)
+
+			elif parentoptions[ran] == 4:
+				parentonright(i, j)
+
+for i in agrid:
+	for j in i:
+		print(j, end = "")
+	print()
---
+++ MAZE/Parentclass.py
@@ -0,0 +1,10 @@
+class Parent:
+
+	def __init__(self, name, parent):
+
+		self.name = name
+		self.parent = parent
+
+	def parent(self):
+
+		return self.parent
